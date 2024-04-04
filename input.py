228b6a48d42db15d3b99c7c4cf33ccb0ff0862a3
Wangari-j <wangarijudy@gmail.com>
2024-04-04 16:41:37 +0300

    kibo

---
+++ input.py
@@ -0,0 +1,28 @@
+print()
+print
+print("Helloo")
+
+#converting inputs
+response = input("How old are you?")
+
+age = int(response)
+
+wedding_date = int(input("When will the wedding take place?"))
+
+#converting float
+price = float(input("How much is the coat?"))
+
+name = "Judy"
+
+print("Hello," + name)
+print(name + " and Alex will go to the market.")
+print(f"{name} and alex")
+
+
+print(f"Hello {name}")
+
+# hi, my name is judy and my age is 16
+print(f"hi, my name is {name} and my age is {age}")
+print("Hi,my name is " + name + " and my age is " + str(age))
+
+print(f"hi, my name is {name} and my age is {age}" )
